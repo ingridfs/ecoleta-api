9a660a1812bf940cb2fad8d06d32a5178402ba0f
ingridfs <ingrid.fernandes.santos@hotmail.com>
2020-06-05 17:23:10 -0300

    Creating Controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,22 @@
-import express, { Router } from 'express';
+import express from 'express';
+import PointsController  from './controllers/PointsController';
+import ItemsController  from './controllers/ItemsController';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {  
-  return response.json({ message: 'Hello World'})
-});
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+        
+routes.get('/points', pointsController.index);
+
+routes.get('/points/:id', pointsController.show);
+
+routes.post('/points', pointsController.create);
+
+routes.delete('/points/:id', pointsController.delete);
+
+routes.put('/points/:id', pointsController.update);
 
 export default routes;
